22f4456eadc526fc34f7b6588368352513382f03
Singh <ssingh@11cw.local>
2020-04-30 10:43:35 -0400

    Initial Commit

---
+++ src/main/java/com/spring/example/SpringProj/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.spring.example.SpringProj;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringProjApplication.class);
+	}
+
+}
---
+++ src/main/java/com/spring/example/SpringProj/SpringProjApplication.java
@@ -0,0 +1,13 @@
+package com.spring.example.SpringProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjApplication.class, args);
+	}
+
+}
